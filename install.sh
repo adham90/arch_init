2a5ad3b5bc37a9379eed66643900142a18e7a680
adham90 <adham.eldeeb90@gmail.com>
2015-07-19 20:09:41 +0200

    edit

--- install.sh
+++ install.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-pacman -Sy xorg-server xorg-server-utils xorg-xinit mesa
-xinit
-cp /etc/X11/xinit/xinitrc ~/.xinitrc
-pacman -Sy awsome vicious xorg-twm xorg-xclock xterm
-pacman -Sy xf86-video-intel libva-intel-driver
-echo "exec awesome" >> ~/.xinitrc
-mkdir -p ~/.config/awesome/
-cp /etc/xdg/awesome/rc.lua ~/.config/awesome/
-pacman -Sy xf86-input-synaptics
-pacman -Syu
+#pacman -Syu
+#pacman -S --noconfirm xorg-server xorg-server-utils xorg-xinit mesa
+#xinit
+#pacman -S --noconfirm vicious xorg-twm xorg-xclock xterm
+#pacman -S --noconfirm xf86-video-intel libva-intel-driver
+#pacman -S --noconfirm xf86-input-synaptics
+#pacman -S --noconfirm ruby
 
+git clone https://github.com/adham90/dotfile.git dotfile
+ruby dotfile/setup.rb
 
